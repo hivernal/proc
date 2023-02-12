eb7ee47181bbf28e19b2d38de673b3172b4ab11c
hivernal <geryaevna@mail.ru>
2023-02-12 16:41:07 +0700

    add checking overflow container

--- container_in.cpp
+++ container_in.cpp
@@ -5,7 +5,7 @@ namespace encryption_methods {
   encryption* in(std::ifstream& in_file);
 
   void in(container& cont, std::ifstream& in_file) {
-    while (in_file.peek() != EOF) {
+    while (in_file.peek() != EOF && cont.len < container::MAX_SIZE) {
       if ((cont.array[cont.len] = in(in_file)) != 0)
         cont.len++;
     }
--- container_out.cpp
+++ container_out.cpp
@@ -5,7 +5,7 @@ namespace encryption_methods {
   void out(encryption* encr, std::ofstream& out_file);
 
   void out(container& cont, std::ofstream& out_file) {
-    for (size_t i = 0; i < cont.len; i++) {
+    for (size_t i = 0; i < cont.len; ++i) {
       out_file << i << ": ";
       out((encryption*) (cont.array[i]), out_file);
     }
