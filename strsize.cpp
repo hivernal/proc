8e5809c8db5fb08c24b3330423138bcd57c70c43
hivernal <geryaevna@mail.ru>
2023-02-25 23:57:42 +0700

    new strsize func

---
+++ strsize.cpp
@@ -0,0 +1,17 @@
+#include "encryption.h"
+
+namespace encryption_methods {
+  size_t str_size(const encryption* en) {
+    switch (en->key) {
+      case encryption::REPLACING:
+        return((replacing*) en)->first_str.size();
+      case encryption::CYCLE:
+        return((cycle*) en)->first_str.size();
+      case encryption::STRTONUM:
+        return((strtonum*) en)->str.size();
+      default:
+        return 0;
+    }
+  }
+}
+
